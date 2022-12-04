bdfeb86f2215be36b00cb010863312d045ff72de
Fida2002 <118302118+Fida2002@users.noreply.github.com>
2022-12-04 13:15:32 +0500

    milestone 3 first commit
    milestone 3 first commit

---
+++ test_wunzip.c
@@ -0,0 +1,47 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+char buf[512];
+
+void
+readingfile(int fd)
+{
+  int n;
+
+  while((n = read(fd, buf, sizeof(buf))) > 0) {
+    if (write(1, buf, n) != n) {
+      printf(2, " write error\n");
+      exit();
+    }
+  }
+  if(n < 0){
+    printf(2, " read error\n");
+    exit();
+  }
+}
+
+int
+main(int argc, char *argv[])
+{
+  int fd, i;
+
+  if(argc <= 1){
+    readingfile(0);
+    exit();
+  }
+
+  for(i = 1; i < argc; i++){
+    if((fd = open(argv[i], 0)) < 0){
+      printf(2, " cannot open %s\n", argv[i]);
+      exit();
+    }
+    readingfile(fd);
+// it will read file  first in binary format  and then unzip file  using 
+// wunzip system call 
+  //  wunzip(fd);
+close(fd);
+  }
+exit();
+}
+
